1255c14a9a5a09daf4cb2ec35bf6355b32cb5166
Vegancat <mmostafavi247@gmail.com>
2026-04-26 15:05:34 +0330

    feat: integrate keyboard controls, zustand, and react-query devtools while updating dev script and loading UI

--- apps/game-web-client/pages/_app.tsx
+++ apps/game-web-client/pages/_app.tsx
@@ -1,8 +1,12 @@
 import "@/styles/globals.css";
-import { ScreenSizeProvider } from "@/contexts/ScreenSizeContext";
-import type { AppProps } from "next/app";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import Head from "next/head";
+import type { AppProps } from "next/app";
+import { ScreenSizeProvider } from "@/contexts/ScreenSizeContext";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { KeyboardControls } from "@react-three/drei";
+import { ToastContainer } from "react-toastify";
+import { useMemo } from "react";
 
 const APP_NAME = "Land of Landless";
 const APP_DESCRIPTION = "Land of Landless";
@@ -10,7 +14,37 @@ const APP_DESCRIPTION = "Land of Landless";
 // query client
 const queryClient = new QueryClient();
 
+type KeyboardControlsEntry<T extends string = string> = {
+  /** Name of the action */
+  name: T;
+  /** The keys that define it, you can use either event.key, or event.code */
+  keys: string[];
+  /** If the event receives the keyup event, true by default */
+  up?: boolean;
+};
+
 export default function App({ Component, pageProps }: AppProps) {
+  enum Controls {
+    forward = "forward",
+    back = "back",
+    left = "left",
+    right = "right",
+    space = "space",
+    shift = "shift",
+  }
+
+  const map = useMemo<KeyboardControlsEntry<Controls>[]>(
+    () => [
+      { name: Controls.forward, keys: ["ArrowUp", "KeyW"] },
+      { name: Controls.back, keys: ["ArrowDown", "KeyS"] },
+      { name: Controls.left, keys: ["ArrowLeft", "KeyA"] },
+      { name: Controls.right, keys: ["ArrowRight", "KeyD"] },
+      { name: Controls.space, keys: ["Space"] },
+      { name: Controls.shift, keys: ["ShiftLeft", "ShiftRight"] },
+    ],
+    [],
+  );
+
   return (
     <>
       <Head>
@@ -34,7 +68,11 @@ export default function App({ Component, pageProps }: AppProps) {
 
       <ScreenSizeProvider>
         <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
+          <KeyboardControls map={map}>
+            <ToastContainer />
+            <Component {...pageProps} />
+          </KeyboardControls>
         </QueryClientProvider>
       </ScreenSizeProvider>
     </>
